94952e603ce76b39aa2d7a5d2e917e0d47d2c21c
ryang218 <RYAN.25@ichat.sp.edu.sg>
2026-04-30 14:31:49 +0800

    BMI calc

---
+++ bmi.py
@@ -0,0 +1,19 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    
+    bmi = weight / (height * height)
+
+    
+    print(f"BMI = {bmi:.2f}" )
+
+    
+    if bmi < 18.5:
+        print("Classification: Under Weight")
+    elif 18.5 <= bmi <= 25.0:
+        print("Classification: Normal Weight")
+    else:
+        print("Classification: Over Weight")
+
+calculate_bmi(weight=57, height=1.73)
