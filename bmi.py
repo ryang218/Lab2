f8a9655160b0333192cb6d8dbf6c9bffa4d82bdd
ryang218 <RYAN.25@ichat.sp.edu.sg>
2026-05-07 13:49:13 +0800

    add return -1/0/1 to classification

--- bmi.py
+++ bmi.py
@@ -11,9 +11,11 @@ def calculate_bmi(height, weight):
     
     if bmi < 18.5:
         print("Classification: Under Weight")
+        return -1 
     elif 18.5 <= bmi <= 25.0:
         print("Classification: Normal Weight")
+        return 0
     else:
         print("Classification: Over Weight")
-
+        return 1 
 calculate_bmi(weight=57, height=1.73)
